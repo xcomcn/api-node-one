df32d3352690c8aae5bbb108778d91ea8eb3d9b9
lu <luqiangbovip@163.com>
2025-07-13 11:58:47 +0800

    测试

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,7 +4,7 @@ const router = express.Router();
 import { resWin } from "../utils/index.js";
 
 router.post("/add", async (req, res) => {
-  return resWin({ res, data: { hello: "word" } });
+  return resWin({ res, data: { hello: "word123" } });
 });
 
 export default router;
